8a39a3a634520b804c12fd5399ac4e78dccedb8a
Peng Huang <huangkouhou@gmail.com>
2025-07-28 18:14:33 +0900

    Order Routes & Order Controller

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,54 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+
+
+//@desc Create new order
+//@route POST/api/orders
+//@access Private
+const addOrderItems = asyncHandler(async(req, res) => {
+    res.send('add order items');
+});
+
+//@desc Get Logged In User Order
+//@route Get /api/orders/my orders
+//@access Private
+const getMyOrders = asyncHandler(async(req, res) => {
+    res.send('get my orders');
+});
+
+//@desc Get order by id
+//@route Get/api/orders/:id
+//@access Private
+const getOrderById = asyncHandler(async(req, res) => {
+    res.send('get order by id');
+});
+
+//@desc update order to paid
+//@route GET /api/orders/:id/pay
+//@access Private
+const updateOrderToPaid = asyncHandler(async(req, res) => {
+    res.send('update order to paid');
+});
+
+//@desc Update order to delivered
+//@route Get /api/orders/:id/delivered
+//@access Private/Admin
+const updateOrderToDelivered = asyncHandler(async(req, res) => {
+    res.send('update order to delivered');
+});
+
+//@desc Get all orders
+//@route Get /api/orders
+//@access Private/Admin
+const getOrders = asyncHandler(async(req, res) => {
+    res.send('get all orders');
+});
+
+
+export {
+    addOrderItems,
+    getMyOrders,
+    getOrderById,
+    updateOrderToPaid,
+    updateOrderToDelivered,
+    getOrders
+};
---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,24 @@
+import express from "express";
+const router = express.Router();
+import { 
+    addOrderItems,
+    getMyOrders,
+    getOrderById,
+    updateOrderToPaid,
+    updateOrderToDelivered,
+    getOrders
+} from "../controllers/orderController.js";
+import { protect, admin } from '../middleware/authMiddleware.js';
+
+//server.js 中，已经定义好了所有 orderRoutes 中定义的路由都会自动加上 /api/orders 作为前缀。
+router.route('/')
+.post(protect, addOrderItems) 
+.get(protect, admin, getOrders);
+router.route('/mine').get(protect, getMyOrders);
+router.route('/:id').get(protect, admin, getOrderById);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
+router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered);
+
+
+
+export default router;
