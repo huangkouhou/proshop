495f0de4b4a61a85189274a69d2b053b842354c6
Peng Huang <huangkouhou@gmail.com>
2025-08-01 16:14:14 +0900

    Admin create new products function

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -15,10 +15,24 @@ export const productsApiSlice = apiSlice.injectEndpoints({
             }),
             keepUnusedDataFor: 5,
         }),
+
+        //Admin
+        createProduct: builder.mutation({
+            query: () => ({
+                url: PRODUCTS_URL,
+                method: 'POST',
+            }),
+            //这一句的作用是 告诉 RTK Query：这个 mutation 执行成功后，和 'Product' 相关的缓存都应该失效（作废），系统会自动重新请求获取更新数据。
+            invalidatesTags: ['Product'],
+        }),
     }),
 });
 
-export const { useGetProductsQuery, useGetProductDetailsQuery } = productsApiSlice;
+export const { 
+    useGetProductsQuery, 
+    useGetProductDetailsQuery, 
+    useCreateProductMutation, 
+} = productsApiSlice;
 
 //RTK Query 提供的 内建函数
 // | 方法名        | 说明                    | 例子           |
